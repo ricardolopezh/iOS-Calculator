679cd50b3760dba17ffeefceade93e860b9ccc5a
Ricardo Lopez <ricardo.lopezh97@gmail.com>
2019-06-06 12:40:07 -0500

    Create main view

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Ricardo López on 6/6/19.
+//  Copyright © 2019 Ricardo Lopez. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // Mark: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Mark: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
